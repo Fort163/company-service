515e7ed5196022eaf0859ac03b4abcbfae2e941c
Fort163 <Front-91@mail.ru>
2023-11-25 14:13:01 +0400

    fix controller

--- src/main/java/com/quick/recording/company/service/controller/CompanyControllerImpl.java
+++ src/main/java/com/quick/recording/company/service/controller/CompanyControllerImpl.java
@@ -22,8 +22,8 @@ public class CompanyControllerImpl implements CompanyController {
     @Autowired
     private UserController userController;
 
-    @GetMapping("/jopa")
-    public ResponseEntity<CompanyDto> getCompany1(@CurrentUser QROAuth2AuthenticatedPrincipal user) {
+    @Override
+    public ResponseEntity<CompanyDto> getCompany() {
         CompanyDto companyDto = new CompanyDto();
         companyDto.setName("Рога копыта");
         companyDto.setUuid(UUID.randomUUID());
@@ -34,24 +34,37 @@ public class CompanyControllerImpl implements CompanyController {
     }
 
     @Override
-    public ResponseEntity<CompanyDto> getCompany(@CurrentUser QROAuth2AuthenticatedPrincipal user) {
-        CompanyDto companyDto = new CompanyDto();
-        companyDto.setName("Рога копыта");
-        companyDto.setUuid(UUID.randomUUID());
-        companyDto.setUsers(userController.usersByCompany(companyDto.getUuid().toString()).getBody());
-        ResponseEntity<UserDto> currentUser = userController.getCurrentUser();
-        companyDto.getUsers().add(currentUser.getBody());
-        return ResponseEntity.ok(companyDto);
-    }
-
-    @Override
-    public ResponseEntity<List<CompanyDto>> getCompanyList(@CurrentUser QROAuth2AuthenticatedPrincipal user) {
+    public ResponseEntity<List<CompanyDto>> getCompanyList() {
         CompanyDto companyDto1 = new CompanyDto();
         companyDto1.setName("Рога копыта");
         companyDto1.setUuid(UUID.randomUUID());
         CompanyDto companyDto2 = new CompanyDto();
         companyDto2.setName("ОТР 2000");
         companyDto2.setUuid(UUID.randomUUID());
-        return ResponseEntity.ok(List.of(companyDto1,companyDto2));
+        CompanyDto companyDto3 = new CompanyDto();
+        companyDto3.setName("Хорошая");
+        companyDto3.setUuid(UUID.randomUUID());
+        CompanyDto companyDto4 = new CompanyDto();
+        companyDto4.setName("Самая важная и самая красивая на свете компания мечты");
+        companyDto4.setUuid(UUID.randomUUID());
+        CompanyDto companyDto5 = new CompanyDto();
+        companyDto5.setName("Техстрой");
+        companyDto5.setUuid(UUID.randomUUID());
+        CompanyDto companyDto6 = new CompanyDto();
+        companyDto6.setName("Про IT");
+        companyDto6.setUuid(UUID.randomUUID());
+        return ResponseEntity.ok(List.of(companyDto1,companyDto2,companyDto3,companyDto4,companyDto5,companyDto6));
     }
+
+    @GetMapping("/1")
+    public ResponseEntity<CompanyDto> getCompany1() {
+        CompanyDto companyDto = new CompanyDto();
+        companyDto.setName("Рога копыта");
+        companyDto.setUuid(UUID.randomUUID());
+        companyDto.setUsers(userController.usersByCompany(companyDto.getUuid().toString()).getBody());
+        ResponseEntity<UserDto> currentUser = userController.getCurrentUser();
+        companyDto.getUsers().add(currentUser.getBody());
+        return ResponseEntity.ok(companyDto);
+    }
+
 }
