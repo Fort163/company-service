9837efe6cbe55c42abc6098f88c14d577cd7b458
Fort163 <Front-91@mail.ru>
2025-03-01 13:14:21 +0400

    add test activity

---
+++ src/test/java/com/quick/recording/company/service/TestAuthHelper.java
@@ -0,0 +1,30 @@
+package com.quick.recording.company.service;
+
+import com.quick.recording.resource.service.security.SSOService;
+import org.springframework.security.access.AuthorizationServiceException;
+
+import java.net.URISyntaxException;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class TestAuthHelper {
+
+    private String token = null;
+
+    public TestAuthHelper(SSOService ssoService) {
+        if(Objects.nonNull(ssoService)) {
+            try {
+                token = ssoService.getSSOResult().token();
+            }
+            catch (NoSuchElementException exception){
+            }
+            catch (URISyntaxException | AuthorizationServiceException exception){
+                exception.printStackTrace();
+            }
+        }
+    }
+
+    public String token() {
+        return token;
+    }
+}
