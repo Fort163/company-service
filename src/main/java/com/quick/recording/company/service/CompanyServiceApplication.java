ff0775ed8ed1543e5b2344443788517a3e4468e3
Fort163 <Front-91@mail.ru>
2025-03-01 13:13:31 +0400

    change app settings

--- src/main/java/com/quick/recording/company/service/CompanyServiceApplication.java
+++ src/main/java/com/quick/recording/company/service/CompanyServiceApplication.java
@@ -2,13 +2,14 @@ package com.quick.recording.company.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com.quick.recording"})
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.quick.recording.gateway.service")
+@RefreshScope
 public class CompanyServiceApplication {
 
 	public static void main(String[] args) {
