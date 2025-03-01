52b732132d76fd20f8555e30067fdcb5f2eee4b6
Fort163 <Front-91@mail.ru>
2025-03-01 13:12:14 +0400

    change mapper to main

--- src/main/java/com/quick/recording/company/service/mapper/ActivityMapper.java
+++ src/main/java/com/quick/recording/company/service/mapper/ActivityMapper.java
@@ -2,26 +2,11 @@ package com.quick.recording.company.service.mapper;
 
 import com.quick.recording.company.service.entity.ActivityEntity;
 import com.quick.recording.gateway.dto.company.ActivityDto;
-import org.mapstruct.BeanMapping;
+import com.quick.recording.gateway.mapper.MainMapper;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface ActivityMapper {
+public interface ActivityMapper extends MainMapper<ActivityEntity, ActivityDto> {
 
-    ActivityEntity toActivityEntity(ActivityDto activityDto);
-
-    ActivityDto toActivityDto(ActivityEntity entity);
-
-    List<ActivityDto> toActivityDtoList(List<ActivityEntity> list);
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    ActivityEntity toActivityEntityWithNull(ActivityDto dto, @MappingTarget ActivityEntity entity);
-
-
-    ActivityEntity toActivityEntity(ActivityDto dto, @MappingTarget ActivityEntity entity);
 
 }
